f95e1b6ffabb8ce5ea0ef7993c28e8783cb8db82
Sam Whited <sam@samwhited.com>
2012-08-12 23:32:11 -0400

    Use minutes date as slug

--- app/models/minutes_page.rb
+++ app/models/minutes_page.rb
@@ -3,7 +3,6 @@ class MinutesPage < Page
 
   def self.new_with_defaults(config = Radiant::Config)
     page = MinutesPage.new
-    page.slug = Time.now.to_i.to_s
     page.parts.concat(self.default_page_parts)
     page.parent_id = MinutesPage.root.try(:id)
     page.status_id = Status[:published].id
