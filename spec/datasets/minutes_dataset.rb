1a9a92964db11941e738f5276c26adfe054fb7aa
Sam Whited <sam@samwhited.com>
2012-08-29 21:30:22 -0400

    Fix broken dataset so specs will run

--- spec/datasets/minutes_dataset.rb
+++ spec/datasets/minutes_dataset.rb
@@ -3,7 +3,7 @@ class MinutesDataset < Dataset::Base
 
   def load
     create_page 'minutes', :slug => 'minutes', :class_name => 'MinutesPage', :parent_id => pages(:home).id do
-      create_page 'general.minutes', :slug => '1331010000', :class_Name => 'MinutesPage' do
+      create_page 'general.minutes', :slug => '1331010000', :class_name => 'MinutesPage' do
         create_page_part 'general_minutes_body', :name => 'General', :content => 'General Minutes'
       end
     end
