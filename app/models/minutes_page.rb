e11f3d02ad3eaa643755fab7e44e6e09a5ffa7d5
Sam Whited <sam@samwhited.com>
2012-08-30 20:04:03 -0400

    Respect `defaults.page.status'
    And remove embarassing test code

--- app/models/minutes_page.rb
+++ app/models/minutes_page.rb
@@ -5,8 +5,7 @@ class MinutesPage < Page
     page = MinutesPage.new
     page.parts.concat(self.default_page_parts)
     page.parent_id = Page.find_by_path(MinutesExtension.minutes_path).try(:id)
-    page.slug = 'testslug'
-    page.title = 'testtitle'
+    page.status = Status[Radiant::Config['defaults.page.status']]
     page
   end
 
